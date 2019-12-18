ad0d017e4c7ff084ed579950edc5463c41fcf2fd
fanxianfan <fanxianfan@foxmail.com>
2019-12-18 12:44:12 +0800

    1.添加环境变量配置 2.提升依赖版本

--- config/config.js
+++ config/config.js
@@ -3,6 +3,12 @@ import routesConfig from './router.config'
 /*系统配置 ：https://umijs.org/config/*/
 export default {
   treeShaking: true,
+  /*配置浏览器最低版本*/
+  targets: {
+    ie: 10,
+  },
+  /*开启 hash 文件后缀*/
+  hash: true,
   /*路由配置：基于react-router实现*/
   routes: routesConfig,
   /*插件列表*/
@@ -16,7 +22,6 @@ export default {
         dynamicImport: {webpackChunkName: true}, //按需加载
         title: 'miaowPanel', //项目标题
         dll: false, //关闭dll功能
-
         routes: { //路由配置
           exclude: [
             /models\//,
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,20 +1,93 @@
 import React, {Component} from 'react';
-import {Row, Col, notification} from 'antd';
+import {
+  Button,
+  Menu,
+  Icon,
+  notification,
+} from 'antd';
+
+const { SubMenu } = Menu;
 
 /**页面布局*/
 class BasicLayout extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state =  {
+      collapsed: false,
+    };
+    console.log("props",props);
+  }
 
   componentDidMount() {
     notification.success({message: "欢迎进入首页"});
   }
 
+  toggleCollapsed = () => {
+    this.setState({collapsed: !this.state.collapsed});
+  };
+
   render() {
     return (
       <>
-        <Row>
-          <Col span={12}>
-          </Col>
-        </Row>
+        <div>
+          <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+            <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+          </Button>
+          <Menu
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            theme="light"
+            inlineCollapsed={this.state.collapsed}
+          >
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="inbox" />
+              <span>Option 3</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                <Icon type="mail" />
+                <span>Navigation One</span>
+              </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                <Icon type="appstore" />
+                <span>Navigation Two</span>
+              </span>
+              }
+            >
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+          </Menu>
+        </div>
+        <div>
+
+        </div>
         {this.props.children}
       </>
     );
