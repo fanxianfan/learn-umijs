8d60de2b9a33b3be893d6586344666d8dda4843f
fanxianfan <fanxianfan@foxmail.com>
2019-12-30 21:53:53 +0800

    1.调整全局滚动条样式

--- src/pages/ToolKitCalendar/CalendarIndex.js
+++ src/pages/ToolKitCalendar/CalendarIndex.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import {Card, Calendar} from 'antd';
+import styles from '@/utils/common.less';
 
 /**
  * 工具箱：生活日历
  * */
 class CalendarIndex extends Component {
+
+  eventOnSelect = (date) => {
+    console.log(date.format('YYYY-MM-DD'));
+  };
+
   render() {
     return (
       <>
-        日历
+        <Card className={styles.m10}>
+          <Calendar onSelect={this.eventOnSelect} />
+        </Card>
       </>
     );
   }
